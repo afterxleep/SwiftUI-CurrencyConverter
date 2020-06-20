306bc808521e69a7e908d26dacb89b7bab4970f8
Daniel Bernal <yo@danielbernal.co>
2020-06-20 17:27:52 -0500

    Loading Remote Data

--- swiftUI-Currencyconvert/ContentView.swift
+++ swiftUI-Currencyconvert/ContentView.swift
@@ -7,9 +7,34 @@
 //
 
 import SwiftUI
+import TinyNetworking
+import Combine
+
+struct FixerData: Codable {
+    var rates: [String:Double]
+}
+
+let latest = Endpoint<FixerData>(json: .get, url: URL(string: "http://data.fixer.io/api/latest?access_key=7a72246dd8a08d23271b1b46979732d7&format=1")!)
+
+final class Resource<A>: ObservableObject {
+        
+    let endpoint: Endpoint<A>
+    var value: A?
+    
+    init(endpoint: Endpoint<A>) {
+        self.endpoint = endpoint
+        reload()
+    }
+    
+    func reload() {
+        URLSession.shared.load(endpoint) { result in
+            self.value = try? result.get()
+        }
+    }
+}
 
 struct Converter: View {
-    let rates: [String: Double] = ["USD": 1.13, "GBP" : 0.089]
+    let rates: [String: Double]
     
     @State var text: String = "100"
     @State var selection: String = "USD"
@@ -55,10 +80,36 @@ struct Converter: View {
     }
 }
 
+struct ProgressIndicator: NSViewRepresentable {
+    
+    func makeNSView(context: NSViewRepresentableContext<ProgressIndicator>) -> NSProgressIndicator {
+        let progressIndicator = NSProgressIndicator()
+        progressIndicator.startAnimation(nil)
+        progressIndicator.style = .spinning
+        return progressIndicator
+    }
+    
+    func updateNSView(_ nsView: NSProgressIndicator, context: Context) {
+        
+    }
+  
+}
+
 struct ContentView: View {
+    @ObservedObject var resource = Resource(endpoint: latest)
     var body: some View {
-        Converter()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Group {
+            if resource.value == nil {
+                VStack {
+                    Text("Loading...")
+                    ProgressIndicator()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else {
+                Converter(rates: resource.value!.rates)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
     }
 }
 
